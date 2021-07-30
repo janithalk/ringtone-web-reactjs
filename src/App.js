7c5a7c0397cc9ebb805b49fbbfae18a59c129157
janithakuruppu <janithakuruppu@gmail.com>
2021-07-30 11:48:18 +0530

    Initial commit - before style

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import { Button } from "antd";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <Button>Click Here</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
